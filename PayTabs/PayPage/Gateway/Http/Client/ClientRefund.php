2cf6e69bcfb487ceb36b37bee84a164215936525
wajih <wajih.km@gmail.com>
2020-07-23 16:30:58 +0400

    Refund functionality (#3)
    Refund functionality
    - add the Refund capability 
    - display the error message in case refund failed

---
+++ PayTabs/PayPage/Gateway/Http/Client/ClientRefund.php
@@ -0,0 +1,53 @@
+<?php
+
+/**
+ * Copyright © 2016 Magento. All rights reserved.
+ * See COPYING.txt for license details.
+ */
+
+namespace PayTabs\PayPage\Gateway\Http\Client;
+
+use Magento\Payment\Gateway\Http\ClientInterface;
+use Magento\Payment\Gateway\Http\TransferInterface;
+use Magento\Payment\Model\Method\Logger;
+use PayTabs\PayPage\Gateway\Http\PaytabsApi;
+
+class ClientRefund implements ClientInterface
+{
+    /**
+     * @var Logger
+     */
+    private $logger;
+
+    /**
+     * @param Logger $logger
+     */
+    public function __construct(Logger $logger)
+    {
+        $this->logger = $logger;
+    }
+
+    /**
+     * Places request to gateway. Returns result as ENV array
+     *
+     * @param TransferInterface $transferObject
+     * @return array
+     */
+    public function placeRequest(TransferInterface $transferObject)
+    {
+        $req_data = $transferObject->getBody();
+        $values = $req_data['params'];
+        $auth = $req_data['auth'];
+
+        $ptApi = PaytabsApi::getInstance($auth['username'], $auth['key']);
+
+        $response = $ptApi->refund($values);
+
+        $this->logger->debug([
+            'request' => $transferObject->getBody(),
+            'response' => (array) $response
+        ]);
+
+        return (array) $response;
+    }
+}
