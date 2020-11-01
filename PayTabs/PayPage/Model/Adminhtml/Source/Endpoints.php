93eedd62438f7def36466ca938df3e0d415197f3
wajih <wajih.km@gmail.com>
2020-11-01 17:32:19 +0400

    Endpoint control

---
+++ PayTabs/PayPage/Model/Adminhtml/Source/Endpoints.php
@@ -0,0 +1,35 @@
+<?php
+
+/**
+ * Copyright © 2016 Magento. All rights reserved.
+ * See COPYING.txt for license details.
+ */
+
+namespace PayTabs\PayPage\Model\Adminhtml\Source;
+
+use PayTabs\PayPage\Gateway\Http\PaytabsApi;
+use PayTabs\PayPage\Gateway\Http\PaytabsCore2;
+
+/**
+ * Class Endpoints
+ */
+class Endpoints implements \Magento\Framework\Option\ArrayInterface
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function toOptionArray()
+    {
+        new PaytabsCore2();
+        $endpoints = PaytabsApi::getEndpoints();
+
+        $endpoints1 = array_map(function ($key, $value) {
+            return [
+                'value' => $key,
+                'label' => $value
+            ];
+        }, array_keys($endpoints), $endpoints);
+
+        return $endpoints1;
+    }
+}
