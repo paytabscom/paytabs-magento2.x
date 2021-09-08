3a09bf22373a245b0e0226585d7df652ab0f12c5
Wajih <wajih.km@gmail.com>
2021-09-08 16:18:27 +0300

    Iraq endpoint & Fawry fix (#20)
    * Update the SDK (2.2.0)
    * Fawry fix

--- PayTabs/PayPage/view/frontend/web/js/view/payment/fawry.js
+++ PayTabs/PayPage/view/frontend/web/js/view/payment/fawry.js
@@ -16,7 +16,7 @@ define(
         'use strict';
         rendererList.push(
             {
-                type: 'atfawry',
+                type: 'fawry',
                 component: 'PayTabs_PayPage/js/view/payment/method-renderer/paypage-method'
             }
         );
