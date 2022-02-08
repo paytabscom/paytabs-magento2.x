c823abfab3e41ceb493d126777e25b63343564bd
Wajih <wajih.km@gmail.com>
2022-02-08 09:39:40 +0400

    Introduce PaytabsHelpers trait

--- PayTabs/PayPage/Controller/PayPage/Callback.php
+++ PayTabs/PayPage/Controller/PayPage/Callback.php
@@ -15,6 +15,7 @@ use PayTabs\PayPage\Gateway\Http\Client\Api;
 use PayTabs\PayPage\Gateway\Http\PaytabsCore;
 use PayTabs\PayPage\Gateway\Http\PaytabsEnum;
 use PayTabs\PayPage\Gateway\Http\PaytabsHelper;
+use PayTabs\PayPage\Gateway\Http\PaytabsHelpers;
 use PayTabs\PayPage\Model\Adminhtml\Source\CurrencySelect;
 use PayTabs\PayPage\Model\Adminhtml\Source\EmailConfig;
 
@@ -25,6 +26,8 @@ use function PayTabs\PayPage\Gateway\Http\paytabs_error_log;
  */
 class Callback extends Action
 {
+    use PaytabsHelpers;
+
     /**
      * @var PageFactory
      */
@@ -225,83 +228,5 @@ class Callback extends Action
         $order->save();
 
         paytabs_error_log("Order {$orderId}, Message [$res_msg]", 1);
-    }
-
-    //
-
-    public function setNewStatus($order, $newStatus)
-    {
-        $order->setState($newStatus)->setStatus($newStatus);
-        $order->addStatusToHistory($newStatus, "Order was set to '$newStatus' as in the admin's configuration.");
-    }
-
-
-    private function invoice($order, $payment)
-    {
-        $canInvoice = $order->canInvoice();
-        if (!$canInvoice) return;
-
-        $invoice = $payment->getCreatedInvoice();
-        if ($invoice) { //} && !$order->getEmailSent()) {
-            $sent = $this->_invoiceSender->send($invoice);
-            $invoiceId = $invoice->getIncrementId();
-            if ($sent) {
-                $order
-                    ->addStatusHistoryComment(
-                        __('You notified customer about invoice #%1.', $invoiceId)
-                    )
-                    ->setIsCustomerNotified(true)
-                    ->save();
-            } else {
-                $order
-                    ->addStatusHistoryComment(
-                        __('Failed to notify the customer about invoice #%1.', $invoiceId)
-                    )
-                    ->setIsCustomerNotified(false)
-                    ->save();
-            }
-        }
-    }
-
-    //
-
-    public function getAmount($payment, $tranCurrency, $tranAmount, $use_order_currency)
-    {
-        $amount = null;
-
-        $orderCurrency = strtoupper($payment->getOrder()->getOrderCurrencyCode());
-        $baseCurrency  = strtoupper($payment->getOrder()->getBaseCurrencyCode());
-        $tranCurrency  = strtoupper($tranCurrency);
-
-        if ($use_order_currency) {
-            if ($orderCurrency != $tranCurrency) {
-                throw Exception('Diff Currency');
-            }
-
-            if ($tranCurrency == $baseCurrency) {
-                $amount = $tranAmount;
-            } else {
-                // Convert Amount to Base
-                $amount = CurrencySelect::convertOrderToBase($payment, $tranAmount);
-
-                $payment->getOrder()
-                    ->addStatusHistoryComment(
-                        __(
-                            'Transaction amount converted to base currency: (%1) = (%2)',
-                            $payment->getOrder()->getOrderCurrency()->format($tranAmount, [], false),
-                            $payment->formatPrice($amount)
-                        )
-                    )
-                    ->setIsCustomerNotified(false)
-                    ->save();
-            }
-        } else {
-            if ($baseCurrency != $tranCurrency) {
-                throw Exception('Diff Currency');
-            }
-            $amount = $tranAmount;
-        }
-
-        return $amount;
     }
 }
---
+++ PayTabs/PayPage/Gateway/Http/PaytabsHelpers.php
@@ -0,0 +1,84 @@
+<?php
+
+namespace PayTabs\PayPage\Gateway\Http;
+
+use PayTabs\PayPage\Model\Adminhtml\Source\CurrencySelect;
+
+
+trait PaytabsHelpers
+{
+
+    function getAmount($payment, $tranCurrency, $tranAmount, $use_order_currency)
+    {
+        $amount = null;
+
+        $orderCurrency = strtoupper($payment->getOrder()->getOrderCurrencyCode());
+        $baseCurrency  = strtoupper($payment->getOrder()->getBaseCurrencyCode());
+        $tranCurrency  = strtoupper($tranCurrency);
+
+        if ($use_order_currency) {
+            if ($orderCurrency != $tranCurrency) {
+                throw Exception('Diff Currency');
+            }
+
+            if ($tranCurrency == $baseCurrency) {
+                $amount = $tranAmount;
+            } else {
+                // Convert Amount to Base
+                $amount = CurrencySelect::convertOrderToBase($payment, $tranAmount);
+
+                $payment->getOrder()
+                    ->addStatusHistoryComment(
+                        __(
+                            'Transaction amount converted to base currency: (%1) = (%2)',
+                            $payment->getOrder()->getOrderCurrency()->format($tranAmount, [], false),
+                            $payment->formatPrice($amount)
+                        )
+                    )
+                    ->setIsCustomerNotified(false)
+                    ->save();
+            }
+        } else {
+            if ($baseCurrency != $tranCurrency) {
+                throw Exception('Diff Currency');
+            }
+            $amount = $tranAmount;
+        }
+
+        return $amount;
+    }
+
+
+    private function invoice($order, $payment)
+    {
+        $canInvoice = $order->canInvoice();
+        if (!$canInvoice) return;
+
+        $invoice = $payment->getCreatedInvoice();
+        if ($invoice) { //} && !$order->getEmailSent()) {
+            $sent = $this->_invoiceSender->send($invoice);
+            $invoiceId = $invoice->getIncrementId();
+            if ($sent) {
+                $order
+                    ->addStatusHistoryComment(
+                        __('You notified customer about invoice #%1.', $invoiceId)
+                    )
+                    ->setIsCustomerNotified(true)
+                    ->save();
+            } else {
+                $order
+                    ->addStatusHistoryComment(
+                        __('Failed to notify the customer about invoice #%1.', $invoiceId)
+                    )
+                    ->setIsCustomerNotified(false)
+                    ->save();
+            }
+        }
+    }
+
+    function setNewStatus($order, $newStatus)
+    {
+        $order->setState($newStatus)->setStatus($newStatus);
+        $order->addStatusToHistory($newStatus, "Order was set to '$newStatus' as in the admin's configuration.");
+    }
+}
