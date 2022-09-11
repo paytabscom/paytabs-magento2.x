6f890f6ee26ee3d2d3600bd6d1f321954e97715f
Arsalan Ajmal <ars4arsalan@gmail.com>
2022-09-11 13:58:40 +0500

    Fxied An exception of undefined constant was thrown for Paytabs, when checking the version number at admin payment method settings, when admin tries switching to a store view from default store.

--- Model/Adminhtml/Source/ModuleVersion.php
+++ Model/Adminhtml/Source/ModuleVersion.php
@@ -1,13 +1,13 @@
 <?php
-
 namespace PayTabs\PayPage\Model\Adminhtml\Source;
 
+use PayTabs\PayPage\Gateway\Http\PaytabsCore;
 
 class ModuleVersion extends \Magento\Config\Block\System\Config\Form\Field
 {
 
     protected function _getElementHtml(\Magento\Framework\Data\Form\Element\AbstractElement $element)
     {
-        return PAYTABS_PAYPAGE_VERSION;
+        return PaytabsCore::getVersion();
     }
 }
